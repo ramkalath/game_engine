acb8388eb907ad661d12c7972dd5c966424f893f
ramkalath <ramkalath@gmail.com>
2018-10-11 04:21:21 +0530

    shader compile link module written

--- 1_sdl2_window.py
+++ 1_sdl2_window.py
@@ -12,19 +12,8 @@ import sdl2
 from OpenGL import GL
 from OpenGL.arrays import vbo
 import numpy as np
+import Shader
 
-vertexShaderProgram = """   #version 330\n
-                            attribute vec2 coord2d;\n
-                            void main() \n
-                            {\n
-                                gl_Position = vec4(coord2d, 0.0, 1.0);\n
-                            }\n"""
-
-fragmentShaderProgram = """ #version 330\n
-                            void main()\n
-                            {\n
-                                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n
-                            }\n"""
 def sdl_poll_events():
     event = sdl2.SDL_Event()
     if sdl2.SDL_PollEvent(ctypes.byref(event)):
@@ -35,8 +24,6 @@ def sdl_poll_events():
         if (event.type == sdl2.SDL_QUIT):
             return False
     return True
-
-
 
 if __name__ == "__main__":
     window_width = 640
@@ -60,31 +47,8 @@ if __name__ == "__main__":
 
     context = sdl2.SDL_GL_CreateContext(window)
     
-    #***************************************************************************
-    # verttex shader
-    vs = GL.glCreateShader(GL.GL_VERTEX_SHADER)
-    GL.glShaderSource(vs, vertexShaderProgram)
-    GL.glCompileShader(vs)
-    if GL.glGetShaderiv(vs, GL.GL_COMPILE_STATUS) != GL.GL_TRUE:
-        print GL.glGetShaderInfoLog(vs)
-
-    # fragment shader
-    fs = GL.glCreateShader(GL.GL_FRAGMENT_SHADER)
-    GL.glShaderSource(fs, fragmentShaderProgram)
-    GL.glCompileShader(fs)
-    if GL.glGetShaderiv(fs, GL.GL_COMPILE_STATUS) != GL.GL_TRUE:
-        print GL.glGetShaderInfoLog(fs)
-
-    # shader program 
-    shaderProgram = GL.glCreateProgram()
-    GL.glAttachShader(shaderProgram, vs)
-    GL.glAttachShader(shaderProgram, fs)
-
-    # link, validate and use
-    GL.glLinkProgram(shaderProgram)
-    print GL.glGetProgramInfoLog(shaderProgram)
-
-    GL.glValidateProgram(shaderProgram)
+    myshader = Shader.shader("./shaders/vertex_shader.vert", "./shaders/fragment_shader.frag")
+    myshader.process_shader()
     #***************************************************************************
     vertices= np.array([-0.5, -0.5, 0.0, 
                          0.5, -0.5, 0.0,
@@ -108,11 +72,10 @@ if __name__ == "__main__":
         GL.glClearColor(0.09, 0.105, 0.11, 1.0);
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
 
-        GL.glUseProgram(shaderProgram) 
+        GL.glUseProgram(myshader.shaderProgram) 
         GL.glBindVertexArray(VAO)
         GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
         GL.glBindVertexArray(0)
 
         running = sdl_poll_events()
         sdl2.SDL_GL_SwapWindow(window)
-
